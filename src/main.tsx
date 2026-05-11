1402f59cd335c87a708d2d84dce2b2a37f37091a
Evin Le <imevinle@gmail.com>
2026-05-11 22:01:25 +1000

    feat: layout constant, CSS variable, and pb-tab-bar utility

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
+import { BOTTOM_TAB_BAR_HEIGHT } from './lib/layout'
 import './index.css'
+
+document.documentElement.style.setProperty('--bottom-tab-bar-height', `${BOTTOM_TAB_BAR_HEIGHT}px`)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
