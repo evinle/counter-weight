332da0767f72180fb112ea3808e20adac3f0ee42
Evin Le <imevinle@gmail.com>
2026-05-23 23:12:47 +1000

    refactor(db): Dexie upgrade callbacks delegate to pure migration functions

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,6 @@
 import Dexie, { type EntityTable } from 'dexie'
 import type { Timer } from './schema'
+import { migrateV1toV2, migrateV2toV3 } from './migrations'
 
 class CounterWeightDB extends Dexie {
   timers!: EntityTable<Timer, 'id'>
@@ -13,17 +14,14 @@ class CounterWeightDB extends Dexie {
       timers: '++id, status, targetDatetime, priority, isFlagged, groupId',
     }).upgrade(tx =>
       tx.table('timers').toCollection().modify(timer => {
-        timer.originalTargetDatetime = timer.targetDatetime
+        Object.assign(timer, migrateV1toV2(timer))
       })
     )
     this.version(3).stores({
       timers: '++id, status, targetDatetime, priority, isFlagged, groupId, syncStatus, serverId, userId',
     }).upgrade(tx =>
       tx.table('timers').toCollection().modify(timer => {
-        timer.serverId = timer.serverId ?? null
-        timer.userId = timer.userId ?? null
-        timer.syncStatus = timer.syncStatus ?? 'synced'
-        timer.version = timer.version ?? null
+        Object.assign(timer, migrateV2toV3(timer))
       })
     )
   }
