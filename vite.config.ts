1078d750c09e169fa0058f3b9e32a326c448d40b
Evin Le <imevinle@gmail.com>
2026-05-25 19:50:22 +1000

    feat: added wrangler for cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   plugins: [
@@ -30,6 +31,7 @@ export default defineConfig({
         globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
       },
     }),
+    cloudflare(),
   ],
   server: {
     host: true,
